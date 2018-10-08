d16fbbca6b589597f50d7ce7df7c631625761746
viniciuscps <barao_si@hotmail.com>
2018-10-08 16:15:56 -0300

    Update blackbox_teste.sh

--- blackbox_teste.sh
+++ blackbox_teste.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# create user
+useradd --no-create-home --shell /bin/false blackbox_exporter
 BLACKBOX_EXPORTER_VERSION="0.12.0"
 wget https://github.com/prometheus/blackbox_exporter/releases/download/v${BLACKBOX_EXPORTER_VERSION}/blackbox_exporter-${BLACKBOX_EXPORTER_VERSION}.linux-amd64.tar.gz
 tar -xzvf blackbox_exporter-${BLACKBOX_EXPORTER_VERSION}.linux-amd64.tar.gz
@@ -9,10 +11,7 @@ rm -rf ~/blackbox_exporter-0.12.0.linux-amd64.tar.gz ~/blackbox_exporter-0.12.0.
 #create a dir 
 sudo mkdir /etc/blackbox_exporter
 
-# create user
-useradd --no-create-home --shell /bin/false blackbox_exporter
-
-sudo chown blackbox_exporter:blackbox_exporter /etc/blackbox_exporter
+sudo chown -R blackbox_exporter:blackbox_exporter /etc/blackbox_exporter
 sudo touch /etc/blackbox_exporter/blackbox.yml
 
 #Inserir em /etc/blackbox_exporter/blackbox.yml
@@ -23,9 +22,28 @@ echo "modules:
     timeout: 5s
     http:      
       valid_status_codes: []
-      method: GET" " >> /etc/blackbox_exporter/blackbox.yml
+      method: GET" >> /etc/blackbox_exporter/blackbox.yml
 
 sudo chown blackbox_exporter:blackbox_exporter /etc/blackbox_exporter/blackbox.yml
+
+############################################
+#Criar o serviço blackbox_exporter.service #
+
+sudo echo "
+[Unit]
+Description=Blackbox Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=blackbox_exporter
+Group=blackbox_exporter
+Type=simple
+ExecStart=/usr/local/bin/blackbox_exporter --config.file /etc/blackbox_exporter/blackbox.yml
+
+[Install]
+WantedBy=multi-user.target " >> vi /etc/systemd/system/blackbox_exporter.service
+
 
 systemctl daemon-reload
 systemctl start blackbox_exporter
@@ -52,6 +70,9 @@ systemctl status blackbox_exporter
         target_label: instance
       - target_label: __address__
         replacement: localhost:9115"
+
+
+
 
 
 ###################FIM###################
