eea94880c60d3f40ce314f8e3e6ca64263b7f6ac
viniciuscps <barao_si@hotmail.com>
2018-10-08 16:50:43 -0300

    Update blackbox_exporter_for_NODES.sh
    Script verificado e funcional

--- blackbox_exporter_for_NODES.sh
+++ blackbox_exporter_for_NODES.sh
@@ -1,45 +1,71 @@
+
 #!/bin/bash
+# create user
+useradd --no-create-home --shell /bin/false blackbox_exporter
 BLACKBOX_EXPORTER_VERSION="0.12.0"
 wget https://github.com/prometheus/blackbox_exporter/releases/download/v${BLACKBOX_EXPORTER_VERSION}/blackbox_exporter-${BLACKBOX_EXPORTER_VERSION}.linux-amd64.tar.gz
 tar -xzvf blackbox_exporter-${BLACKBOX_EXPORTER_VERSION}.linux-amd64.tar.gz
-cd blackbox_exporter-${BLACKBOX_EXPORTER_VERSION}.linux-amd64
-# não se faz necessário make ./blackbox_exporter
-cp blackbox_exporter /usr/local/bin
+sudo mv ./blackbox_exporter-0.12.0.linux-amd64/blackbox_exporter /usr/local/bin
+sudo chown blackbox_exporter:blackbox_exporter /usr/local/bin/blackbox_exporter
+rm -rf ~/blackbox_exporter-0.12.0.linux-amd64.tar.gz ~/blackbox_exporter-0.12.0.linux-amd64
 
-# create user
-useradd --no-create-home --shell /bin/false blackbox_exporter
+#create a dir 
+sudo mkdir /etc/blackbox_exporter
 
-chown blackbox_exporter:blackbox_exporter /usr/local/bin/blackbox_exporter
+sudo chown -R blackbox_exporter:blackbox_exporter /etc/blackbox_exporter
+sudo touch /etc/blackbox_exporter/blackbox.yml
+
+#Inserir em /etc/blackbox_exporter/blackbox.yml
+echo "modules:
+  http_2xx:
+    prober: http
+    timeout: 5s
+    http:      
+      valid_status_codes: []
+      method: GET" >> /etc/blackbox_exporter/blackbox.yml
+
+sudo chown blackbox_exporter:blackbox_exporter /etc/blackbox_exporter/blackbox.yml
+
+############################################
+#Criar o serviço blackbox_exporter.service #
 
 echo '[Unit]
-Description=BlackBox Exporter
+Description=Blackbox Exporter
 Wants=network-online.target
 After=network-online.target
 [Service]
 User=blackbox_exporter
 Group=blackbox_exporter
 Type=simple
-ExecStart=/usr/local/bin/blackbox_exporter
+ExecStart=/usr/local/bin/blackbox_exporter --config.file /etc/blackbox_exporter/blackbox.yml
 [Install]
 WantedBy=multi-user.target' > /etc/systemd/system/blackbox_exporter.service
 
-## Após a instalação no PROMETHEUS-SERVER inserir as configurações sobre os NODES e serviços que serão raspados ##
 
-##NO NODE blackbox_exporter.yml##
-#caso o arquivo não seja encontrado verificar se o nome é blackbox.yml
-#ou se se faz necessário criar o arquivo antes touch blacbox_exporter.yml
-echo "modules:
-  dcs_results:
-    prober: http
-    timeout: 5s
-    http:
-      fail_if_not_matches_regexp:
-      - "loginmanualunidade.php" " >> /root/blackbox_exporter-0.12.0.linux-amd64/blackbox.yml
-
-#inserido no fim para testar o start somente após a configuração
-# enable blackbox_exporter in systemctl
 systemctl daemon-reload
 systemctl start blackbox_exporter
 systemctl enable blackbox_exporter
+systemctl status blackbox_exporter
 
-#FIM DA EXECUÇÃO
+###################################################
+## INSERIR NO PROMETHEUS-SERVER (prometheus.yml) ##
+###################################################
+
+"sudo vi /etc/prometheus/prometheus.yml
+ - job_name: 'blackbox'
+    metrics_path: /probe
+    params:
+      module: [http_2xx]
+    static_configs:
+      - targets:
+        - http://localhost:8080
+    relabel_configs:
+      - source_labels: [__address__]
+        target_label: __param_target
+      - source_labels: [__param_target]
+        target_label: instance
+      - target_label: __address__
+        replacement: localhost:9115"
+
+
+###################FIM###################
