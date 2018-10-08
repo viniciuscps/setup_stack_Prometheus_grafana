10fcdb5a6b505b3f51e184656038d1f227b88ca9
viniciuscps <barao_si@hotmail.com>
2018-10-08 13:24:02 -0300

    Update blackbox_exporter_for_NODES.sh

--- blackbox_exporter_for_NODES.sh
+++ blackbox_exporter_for_NODES.sh
@@ -3,7 +3,7 @@ BLACKBOX_EXPORTER_VERSION="0.12.0"
 wget https://github.com/prometheus/blackbox_exporter/releases/download/v${BLACKBOX_EXPORTER_VERSION}/blackbox_exporter-${BLACKBOX_EXPORTER_VERSION}.linux-amd64.tar.gz
 tar -xzvf blackbox_exporter-${BLACKBOX_EXPORTER_VERSION}.linux-amd64.tar.gz
 cd blackbox_exporter-${BLACKBOX_EXPORTER_VERSION}.linux-amd64
-make ./blackbox_exporter
+# não se faz necessário make ./blackbox_exporter
 cp blackbox_exporter /usr/local/bin
 
 # create user
@@ -23,34 +23,23 @@ ExecStart=/usr/local/bin/blackbox_exporter
 [Install]
 WantedBy=multi-user.target' > /etc/systemd/system/blackbox_exporter.service
 
-# enable blackbox_exporter in systemctl
-systemctl daemon-reload
-systemctl start blackbox_exporter
-systemctl enable blackbox_exporter
-
 ## Após a instalação no PROMETHEUS-SERVER inserir as configurações sobre os NODES e serviços que serão raspados ##
 
 ##NO NODE blackbox_exporter.yml##
 #caso o arquivo não seja encontrado verificar se o nome é blackbox.yml
 #ou se se faz necessário criar o arquivo antes touch blacbox_exporter.yml
-echo "
-modules:
+echo "modules:
   dcs_results:
     prober: http
     timeout: 5s
     http:
       fail_if_not_matches_regexp:
-      - "loginmanualunidade.php" "
+      - "loginmanualunidade.php" " >> /root/blackbox_exporter-0.12.0.linux-amd64/blackbox.yml
 
-##NO PROMETHEUS-SERVER prometheus.yml##
-- job_name: blackbox_dcs_results
-  scrape_interval: 60s
-  metrics_path: /probe
-  params:
-    module: [dcs_results]
-    target: ['https://avaeduc.com.br']
-  scheme: http
-  target_groups:
-  - targets:
-    - blackbox-exporter:9115 #caso não funcione inserir o nome do servidor que contem a instalação do exportador
-                             #liberar a porta 9115 na AWS (para o node que será monitorado)
+#inserido no fim para testar o start somente após a configuração
+# enable blackbox_exporter in systemctl
+systemctl daemon-reload
+systemctl start blackbox_exporter
+systemctl enable blackbox_exporter
+
+#FIM DA EXECUÇÃO
